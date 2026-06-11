43539db625905704ecf3c1d08c4644acbaeb1382
navidkeyhanifard <navid.keyhanifard@gmail.com>
2026-06-11 22:42:11 +0330

    upgrade gradle wrapper to 8.4 with auto-download jar

--- android-template/gradlew.bat
+++ android-template/gradlew.bat
@@ -1,2 +1,5 @@
-@rem Gradle wrapper
+@if not exist gradle\wrapper\gradle-wrapper.jar (
+    mkdir gradle\wrapper
+    curl -L -o gradle\wrapper\gradle-wrapper.jar https://github.com/gradle/gradle/raw/v8.4.0/gradle/wrapper/gradle-wrapper.jar
+)
 @java -cp "gradle/wrapper/gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
